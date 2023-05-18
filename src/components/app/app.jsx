8e018742873972897f61045872d70ed6653e7982
22Sunatullo <muslimamuminova6969@gmail.com>
2023-05-18 22:54:54 +0500

    First commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header } from "../header/header";
+import RandomPlanet from "../random-planet/random-planet";
+import ItemList from "../item-list/item-list";
+
+import './app.css'
+import PersonDetails from "../person-deteils/person-deteils";
+
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6 mt-3">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails/>
+        </div>
+      </div>
+    </div>
+  );
+};
