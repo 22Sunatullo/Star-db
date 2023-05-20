addeb214a668dc7f916389e1fed8c6ab47a89d2e
22Sunatullo <muslimamuminova6969@gmail.com>
2023-05-20 21:47:06 +0500

    spinner

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./spinner.css";
+
+export const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-od5sspk4gw">
+      <div class="ldio-9nc1h5nflsk">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
